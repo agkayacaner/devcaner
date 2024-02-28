e95903c72625d70381305fc9add5d18be334de2f
agkayacaner <dev.caneragkaya@gmail.com>
2024-02-28 13:32:36 +0300

    Updated Cat Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,7 @@ export default function Home() {
       <div className="text-center mb-10 h-96 flex flex-col justify-center items-center">
         <h1 className="text-7xl font-bold mb-5">Full Stack Developer</h1>
         <h2 className="text-3xl mb-10">CANER AĞKAYA</h2>
-        <iframe
-          src="https://giphy.com/embed/RddaRQbuBgcgw"
-          width="480"
-          height="480"
-          allowFullScreen
-        ></iframe>
+        <Image src="/cat.webp" width="300" height="300" alt="cat" />
       </div>
     </main>
   );
