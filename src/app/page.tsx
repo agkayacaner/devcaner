c4710a6bb7a1823b7ef96d964b9b2809577381df
agkayacaner <dev.caneragkaya@gmail.com>
2024-02-28 13:43:19 +0300

    Added Swift, Firebase, React icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,11 @@ export default function Home() {
         <h1 className="text-7xl font-bold mb-5">Full Stack Developer</h1>
         <h2 className="text-3xl mb-10">CANER AĞKAYA</h2>
         <Image src="/cat.webp" width="300" height="300" alt="cat" />
+        <div className="flex gap-5 mt-20">
+          <Image src="/swift.png" width="32" height="32" alt="cat" />
+          <Image src="/firebase.svg" width="32" height="32" alt="cat" />
+          <Image src="/react.svg" width="32" height="32" alt="cat" />
+        </div>
       </div>
     </main>
   );
